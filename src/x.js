6db7fb4e56dcef8b033d6e405a2ef7d44c60979a
ReformedCola <reformedcola@gmail.com>
2020-03-31 10:04:52 -0700

    update

--- src/x.js
+++ src/x.js
@@ -1,2 +1,2 @@
-
+import from './x.css'
 export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Webpack',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+    ],
+  },
 };
