178f97d3744e275982d1424fb254840723378575
ReformedCola <reformedcola@gmail.com>
2020-04-01 10:01:03 -0700

    ready to upload

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ const div = document.getElementById('app')
 div.innerHTML = `
   <img src="${png}">
 `
+
+const button = document.createElement('button')
+button.innerText = 'Lazy Loading'
+button.onclick = () => {
+  const promise = import('./lazy')
+  promise.then((module) => {
+    const fn = module.default
+    fn()
+  }, () => {
+    console.log('Fail to load module')
+  })
+}
+
+div.appendChild(button)
